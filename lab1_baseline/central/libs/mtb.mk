6316bef2534ed3eb90101461e3afcc59ab141419
Mohammad Afaneh <mafaneh@gmail.com>
2026-02-22 01:14:50 -0500

    Include BSP in all projects to fix ModusToolbox import
    - Add bsps/TARGET_CYW920829M2EVK-02/ to all 6 project directories
      (make getlibs does not fetch BSPs; they must be committed)
    - Standardize TARGET to CYW920829M2EVK-02 in all central Makefiles
      (was APP_CYW920829M2EVK-02 which didn't match BSP directory name)
    - Fix design.cybt device from "CYW20829" to "43xxx" in all central
      projects (bt-configurator 3.40 requires "43xxx" device identifier)
    - Update mtb.mk files with BSP search path and BSP dependency entries
    - Add peripheral libs/mtb.mk files (were missing)
    - Update .gitignore: allow bsps/, ignore IDE-generated files
    
    Without these changes, `make eclipse` fails with:
      Libraries: "bsp core-make recipe-make" not found.

--- lab1_baseline/central/libs/mtb.mk
+++ lab1_baseline/central/libs/mtb.mk
@@ -6,15 +6,24 @@
 # List of local libraries
 
 
+# Path to the current BSP
+SEARCH_TARGET_CYW920829M2EVK-02=bsps/TARGET_CYW920829M2EVK-02
 
 # The search paths for the included middleware
-SEARCH_btstack-integration=../mtb_shared/btstack-integration/latest-v7.X
-SEARCH_display-oled-ssd1306=../mtb_shared/display-oled-ssd1306/release-v1.0.0
-SEARCH_retarget-io=../mtb_shared/retarget-io/latest-v1.X
-SEARCH_abstraction-rtos=../mtb_shared/abstraction-rtos/release-v1.12.0
-SEARCH_btstack=../mtb_shared/btstack/release-v5.0.2
-SEARCH_clib-support=../mtb_shared/clib-support/release-v1.8.0
-SEARCH_freertos=../mtb_shared/freertos/release-v10.6.202
+SEARCH_btstack-integration=../../mtb_shared/btstack-integration/latest-v7.X
+SEARCH_display-oled-ssd1306=../../mtb_shared/display-oled-ssd1306/release-v1.0.0
+SEARCH_retarget-io=../../mtb_shared/retarget-io/latest-v1.X
+SEARCH_abstraction-rtos=../../mtb_shared/abstraction-rtos/release-v1.12.0
+SEARCH_btstack=../../mtb_shared/btstack/release-v5.0.4
+SEARCH_clib-support=../../mtb_shared/clib-support/release-v1.8.0
+SEARCH_cmsis=../../mtb_shared/cmsis/release-v5.8.2
+SEARCH_core-lib=../../mtb_shared/core-lib/release-v1.6.0
+SEARCH_core-make=../../mtb_shared/core-make/release-v3.8.0
+SEARCH_freertos=../../mtb_shared/freertos/release-v10.6.202
+SEARCH_fw-cyw20829b1=../../mtb_shared/fw-cyw20829b1/release-v1.0.0
+SEARCH_mtb-hal-cat1=../../mtb_shared/mtb-hal-cat1/release-v2.7.4
+SEARCH_mtb-pdl-cat1=../../mtb_shared/mtb-pdl-cat1/release-v3.19.0
+SEARCH_recipe-make-cat1b=../../mtb_shared/recipe-make-cat1b/release-v2.8.2
 
 # Search libraries added to build
 SEARCH_MTB_MK+=$(SEARCH_btstack-integration)
@@ -23,7 +32,14 @@ SEARCH_MTB_MK+=$(SEARCH_retarget-io)
 SEARCH_MTB_MK+=$(SEARCH_abstraction-rtos)
 SEARCH_MTB_MK+=$(SEARCH_btstack)
 SEARCH_MTB_MK+=$(SEARCH_clib-support)
+SEARCH_MTB_MK+=$(SEARCH_cmsis)
+SEARCH_MTB_MK+=$(SEARCH_core-lib)
+SEARCH_MTB_MK+=$(SEARCH_core-make)
 SEARCH_MTB_MK+=$(SEARCH_freertos)
+SEARCH_MTB_MK+=$(SEARCH_fw-cyw20829b1)
+SEARCH_MTB_MK+=$(SEARCH_mtb-hal-cat1)
+SEARCH_MTB_MK+=$(SEARCH_mtb-pdl-cat1)
+SEARCH_MTB_MK+=$(SEARCH_recipe-make-cat1b)
 
 -include $(CY_INTERNAL_APP_PATH)/importedbsp.mk
 COMPONENTS += MW_BTSTACK_INTEGRATION
@@ -32,11 +48,147 @@ COMPONENTS += MW_RETARGET_IO
 COMPONENTS += MW_ABSTRACTION_RTOS
 COMPONENTS += MW_BTSTACK
 COMPONENTS += MW_CLIB_SUPPORT
+COMPONENTS += MW_CMSIS
+COMPONENTS += MW_CORE_LIB
+COMPONENTS += MW_CORE_MAKE
 COMPONENTS += MW_FREERTOS
+COMPONENTS += MW_FW_CYW20829B1
+COMPONENTS += MW_MTB_HAL_CAT1
+COMPONENTS += MW_MTB_PDL_CAT1
+COMPONENTS += MW_RECIPE_MAKE_CAT1B
+
+# Register map file
+DEVICE_CYW20829B1010_SVD=$(SEARCH_mtb-pdl-cat1)/devices/COMPONENT_CAT1B/svd/cyw20829.svd
 
 
 #
 # generate make targets for the graphical editors that are specific to this project
 #
+
+modlibs:
+	$(CY_TOOL_mtblaunch_EXE_ABS) --project . --short-name library-manager
+.PHONY: modlibs
+
+CY_HELP_modlibs=Launches the Library Manager 2.50 GUI
+CY_HELP_modlibs_VERBOSE=Launches the Library Manager 2.50 GUI. Check the Library Manager 2.50 User Guide for more information.
+mtb_help_tool_modlibs:
+	@:
+	$(info $(MTB__SPACE)modlibs             $(CY_HELP_modlibs))
+
+mtb_help_tools_end: mtb_help_tool_modlibs
+mtb_help_tool_modlibs: mtb_help_tools_start
+.PHONY: mtb_help_tool_modlibs
+
+library-manager:
+	$(CY_TOOL_mtblaunch_EXE_ABS) --project . --short-name library-manager
+.PHONY: library-manager
+
+CY_HELP_library-manager=Launches the Library Manager 2.50 GUI
+CY_HELP_library-manager_VERBOSE=Launches the Library Manager 2.50 GUI. Check the Library Manager 2.50 User Guide for more information.
+mtb_help_tool_library-manager:
+	@:
+	$(info $(MTB__SPACE)library-manager     $(CY_HELP_library-manager))
+
+mtb_help_tools_end: mtb_help_tool_library-manager
+mtb_help_tool_library-manager: mtb_help_tools_start
+.PHONY: mtb_help_tool_library-manager
+
+qspi-configurator:
+	$(CY_TOOL_mtblaunch_EXE_ABS) --project . --short-name qspi-configurator
+.PHONY: qspi-configurator
+
+CY_HELP_qspi-configurator=Launches the QSPI Configurator 4.60 GUI for the target's cyqspi file
+CY_HELP_qspi-configurator_VERBOSE=Launches the QSPI Configurator 4.60 GUI. Check the QSPI Configurator 4.60 User Guide for more information.
+mtb_help_tool_qspi-configurator:
+	@:
+	$(info $(MTB__SPACE)qspi-configurator   $(CY_HELP_qspi-configurator))
+
+mtb_help_tools_end: mtb_help_tool_qspi-configurator
+mtb_help_tool_qspi-configurator: mtb_help_tools_start
+.PHONY: mtb_help_tool_qspi-configurator
+
+config:
+	$(CY_TOOL_mtblaunch_EXE_ABS) --project . --short-name device-configurator
+.PHONY: config
+
+CY_HELP_config=Launches the Device Configurator 5.50 GUI for the target's modus file
+CY_HELP_config_VERBOSE=Launches the Device Configurator 5.50 GUI. Check the Device Configurator 5.50 User Guide for more information.
+mtb_help_tool_config:
+	@:
+	$(info $(MTB__SPACE)config              $(CY_HELP_config))
+
+mtb_help_tools_end: mtb_help_tool_config
+mtb_help_tool_config: mtb_help_tools_start
+.PHONY: mtb_help_tool_config
+
+device-configurator:
+	$(CY_TOOL_mtblaunch_EXE_ABS) --project . --short-name device-configurator
+.PHONY: device-configurator
+
+CY_HELP_device-configurator=Launches the Device Configurator 5.50 GUI for the target's modus file
+CY_HELP_device-configurator_VERBOSE=Launches the Device Configurator 5.50 GUI. Check the Device Configurator 5.50 User Guide for more information.
+mtb_help_tool_device-configurator:
+	@:
+	$(info $(MTB__SPACE)device-configurator $(CY_HELP_device-configurator))
+
+mtb_help_tools_end: mtb_help_tool_device-configurator
+mtb_help_tool_device-configurator: mtb_help_tools_start
+.PHONY: mtb_help_tool_device-configurator
+
+bsp-assistant:
+	$(CY_TOOL_mtblaunch_EXE_ABS) --project . --short-name bsp-assistant
+.PHONY: bsp-assistant
+
+CY_HELP_bsp-assistant=Launches the BSP Assistant 1.50 GUI
+CY_HELP_bsp-assistant_VERBOSE=Launches the BSP Assistant 1.50 GUI. Check the BSP Assistant 1.50 User Guide for more information.
+mtb_help_tool_bsp-assistant:
+	@:
+	$(info $(MTB__SPACE)bsp-assistant       $(CY_HELP_bsp-assistant))
+
+mtb_help_tools_end: mtb_help_tool_bsp-assistant
+mtb_help_tool_bsp-assistant: mtb_help_tools_start
+.PHONY: mtb_help_tool_bsp-assistant
+
+config_bt:
+	$(CY_TOOL_mtblaunch_EXE_ABS) --project . --short-name bt-configurator
+.PHONY: config_bt
+
+CY_HELP_config_bt=Launches the Bluetooth® Configurator 3.40 GUI for the target's cybt file
+CY_HELP_config_bt_VERBOSE=Launches the Bluetooth® Configurator 3.40 GUI. Check the Bluetooth® Configurator 3.40 User Guide for more information.
+mtb_help_tool_config_bt:
+	@:
+	$(info $(MTB__SPACE)config_bt           $(CY_HELP_config_bt))
+
+mtb_help_tools_end: mtb_help_tool_config_bt
+mtb_help_tool_config_bt: mtb_help_tools_start
+.PHONY: mtb_help_tool_config_bt
+
+bt-configurator:
+	$(CY_TOOL_mtblaunch_EXE_ABS) --project . --short-name bt-configurator
+.PHONY: bt-configurator
+
+CY_HELP_bt-configurator=Launches the Bluetooth® Configurator 3.40 GUI for the target's cybt file
+CY_HELP_bt-configurator_VERBOSE=Launches the Bluetooth® Configurator 3.40 GUI. Check the Bluetooth® Configurator 3.40 User Guide for more information.
+mtb_help_tool_bt-configurator:
+	@:
+	$(info $(MTB__SPACE)bt-configurator     $(CY_HELP_bt-configurator))
+
+mtb_help_tools_end: mtb_help_tool_bt-configurator
+mtb_help_tool_bt-configurator: mtb_help_tools_start
+.PHONY: mtb_help_tool_bt-configurator
+
+smartio-configurator:
+	$(CY_TOOL_mtblaunch_EXE_ABS) --project . --short-name smartio-configurator
+.PHONY: smartio-configurator
+
+CY_HELP_smartio-configurator=Launches the Smart I/O Configurator 4.50 GUI for the target's modus file
+CY_HELP_smartio-configurator_VERBOSE=Launches the Smart I/O Configurator 4.50 GUI. Check the Smart I/O Configurator 4.50 User Guide for more information.
+mtb_help_tool_smartio-configurator:
+	@:
+	$(info $(MTB__SPACE)smartio-configurator $(CY_HELP_smartio-configurator))
+
+mtb_help_tools_end: mtb_help_tool_smartio-configurator
+mtb_help_tool_smartio-configurator: mtb_help_tools_start
+.PHONY: mtb_help_tool_smartio-configurator
 
 .PHONY: mtb_help_tools_start mtb_help_tools_end
